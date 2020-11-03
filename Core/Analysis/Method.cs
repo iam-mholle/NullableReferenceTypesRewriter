7905a95048f5adeeb0bea9ac2d52f487b9517dfc
Marcel Holle <marcel.holle@rubicon.eu>
2020-11-03 10:42:59 +0100

    Add Method node rewrite method

--- Core/Analysis/Method.cs
+++ Core/Analysis/Method.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace NullableReferenceTypesRewriter.Analysis
@@ -14,12 +15,20 @@ namespace NullableReferenceTypesRewriter.Analysis
     public MethodDeclarationSyntax MethodDeclaration => (MethodDeclarationSyntax) _methodSyntaxReference.GetSyntax();
     public IReadOnlyCollection<Dependency> Parents => _parents();
     public IReadOnlyCollection<Dependency> Children => _children();
+    public MethodDeclarationSyntax SyntaxNode { get; private set; }
 
     public Method (MethodDeclarationSyntax methodDeclaration, Func<IReadOnlyCollection<Dependency>> parents, Func<IReadOnlyCollection<Dependency>> children)
     {
+      SyntaxNode = methodDeclaration;
       _methodSyntaxReference = methodDeclaration.GetReference();
       _parents = parents;
       _children = children;
+    }
+
+    public void Rewrite (CSharpSyntaxRewriter rewriter)
+    {
+      SyntaxNode = (MethodDeclarationSyntax) (rewriter.VisitMethodDeclaration (SyntaxNode)
+                                              ?? throw new InvalidOperationException ($"Could not rewrite {_methodSyntaxReference.GetSyntax().ToString()}."));
     }
   }
 }
