034175ace48233c9330207607bc147890c6ac6e5
Marcel Holle <marcel.holle@rubicon.eu>
2020-11-24 14:32:49 +0100

    Counter circular graph walking

--- Core/Analysis/ChildrenMemberGraphVisitor.cs
+++ Core/Analysis/ChildrenMemberGraphVisitor.cs
@@ -1,15 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace NullableReferenceTypesRewriter.Analysis
 {
   public abstract class ChildrenMemberGraphVisitor : MemberGraphVisitorBase
   {
+    private readonly Stack<Method> _visitedMethods = new Stack<Method>();
+
     public override void VisitMethod (Method method)
     {
+      if (_visitedMethods.Contains(method))
+      {
+        return;
+      }
+
+      _visitedMethods.Push (method);
+
       foreach (var child in method.Children)
       {
         child.To.Accept (this);
       }
+
+      _visitedMethods.Pop();
     }
 
     public override void VisitExternalMethod (ExternalMethod externalMethod)
--- Core/Analysis/ParentMemberGraphVisitor.cs
+++ Core/Analysis/ParentMemberGraphVisitor.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace NullableReferenceTypesRewriter.Analysis
 {
   public abstract class ParentMemberGraphVisitor : MemberGraphVisitorBase
   {
+    private readonly Stack<Method> _visitedMethods = new Stack<Method>();
+
     public override void VisitMethod (Method method)
     {
+      if (_visitedMethods.Contains(method))
+      {
+        return;
+      }
+
+      _visitedMethods.Push(method);
+
       VisitParents (method);
+
+      _visitedMethods.Pop();
     }
 
     public override void VisitExternalMethod (ExternalMethod externalMethod)
