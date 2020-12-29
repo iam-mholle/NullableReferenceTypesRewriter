38e78615dfb52280e5020da58d4116ee6c421ef8
Marcel Holle <privat@mholle.com>
2020-12-29 15:44:10 +0100

    More tests

--- Core/Analysis/UninitializedFieldRewriter.cs
+++ Core/Analysis/UninitializedFieldRewriter.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NullableReferenceTypesRewriter.ClassFields;
+using NullableReferenceTypesRewriter.Utilities;
 
 namespace NullableReferenceTypesRewriter.Analysis
 {
@@ -13,14 +16,51 @@ namespace NullableReferenceTypesRewriter.Analysis
     {
     }
 
-    public override SyntaxNode? VisitClassDeclaration (ClassDeclarationSyntax node)
+    public override SyntaxNode? VisitFieldDeclaration (FieldDeclarationSyntax node)
     {
       var semanticModel = _currentField.SemanticModel;
-      var fields = new FieldLocator (node, semanticModel).LocateFields();
 
-      var uninitializedFields = new ConstructorInitializationFilter (node, fields).GetUnitializedFields();
+      if (IsValueType(semanticModel, node))
+        return node;
 
-      return new FieldNullableAnnotator (node, uninitializedFields).AnnotateFields();
+      if (IsNullable(semanticModel, node))
+        return node;
+
+      if (node.Declaration.Variables.Any(d => IsInitializedToNull(semanticModel, d)))
+      {
+        return node.WithDeclaration(node.Declaration.WithType(NullUtilities.ToNullable(node.Declaration.Type)));
+      }
+
+      // TODO: check if ctor initialized to nullable -> nullable
+      // TODO: check if uninitialized in one ctor -> nullable
+
+      return node;
+    }
+
+    private bool IsReadOnly (MemberDeclarationSyntax field)
+      => field.Modifiers.FirstOrDefault (mod => mod.ToString() == "readonly").Kind() != SyntaxKind.None;
+
+    private bool HasNoInitializer (VariableDeclaratorSyntax variableDeclarator)
+      => variableDeclarator.Initializer == null;
+
+    private bool IsValueType (SemanticModel semanticModel, FieldDeclarationSyntax declaration)
+    {
+      var typeSymbol = semanticModel.GetTypeInfo (declaration.Declaration.Type).Type as INamedTypeSymbol;
+      return typeSymbol == null
+             || typeSymbol.IsValueType;
+    }
+
+    private bool IsNullable(SemanticModel semanticModel, FieldDeclarationSyntax syntax)
+      => (semanticModel.GetDeclaredSymbol(syntax.Declaration.Variables.First()) as IFieldSymbol)?.Type.NullableAnnotation == NullableAnnotation.Annotated;
+
+    private bool IsInitializedToNull (SemanticModel semanticModel, VariableDeclaratorSyntax variableDeclarator)
+    {
+      if (variableDeclarator.Initializer != null)
+      {
+        return NullUtilities.CanBeNull (variableDeclarator.Initializer.Value, semanticModel);
+      }
+
+      return false;
     }
   }
 }
--- Unittests/Analysis/UninitializedFieldRewriterTest.cs
+++ Unittests/Analysis/UninitializedFieldRewriterTest.cs
@@ -13,17 +13,61 @@ namespace NullableReferenceTypesRewriter.UnitTests.Analysis
   public class UninitializedFieldRewriterTest
   {
     [Test]
-    public void Test ()
+    public void InlineInitialized_ToNull_Nullable ()
     {
       //language=C#
       const string expected = @"
-private string? test;
+private string? test = null;
 ";
       var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
           "A",
           //language=C#
           @"
-private string test;
+private string test = null;
+");
+      var syntax = (FieldDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.FieldDeclaration));
+      var field = CreateFieldWrapper (syntax, semantic);
+      var sut = new UninitializedFieldRewriter((b, c) => { });
+
+      var result = sut.Rewrite (field);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void InlineInitialized_ToNotNull_Unchanged ()
+    {
+      //language=C#
+      const string expected = @"
+private string test = string.Empty;
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+private string test = string.Empty;
+");
+      var syntax = (FieldDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.FieldDeclaration));
+      var field = CreateFieldWrapper (syntax, semantic);
+      var sut = new UninitializedFieldRewriter((b, c) => { });
+
+      var result = sut.Rewrite (field);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void AlreadyNullable_Unchanged ()
+    {
+      //language=C#
+      const string expected = @"
+private string? test = string.Empty;
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+private string? test = string.Empty;
 ");
       var syntax = (FieldDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.FieldDeclaration));
       var field = CreateFieldWrapper (syntax, semantic);
@@ -41,7 +85,7 @@ private string test;
     {
       return new Field(
           new SharedCompilation(semanticModel.Compilation),
-          (IFieldSymbol) semanticModel.GetDeclaredSymbol(syntax.Declaration.Variables.Single()),
+          (IFieldSymbol) semanticModel.GetDeclaredSymbol(syntax.Declaration.Variables.Single())!,
           parents ?? Array.Empty<Dependency>);
     }
   }
