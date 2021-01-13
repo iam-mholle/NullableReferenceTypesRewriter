6c43668734d26c3c1cfcca6fe819a8d8d31bfd43
Marcel Holle <privat@mholle.com>
2021-01-13 14:13:16 +0100

    Add MethodArgumentRewriter

---
+++ Core/Analysis/MethodArgumentRewriter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NullableReferenceTypesRewriter.Utilities;
+
+namespace NullableReferenceTypesRewriter.Analysis
+{
+  public class MethodArgumentRewriter : RewriterBase
+  {
+    public MethodArgumentRewriter(Action<RewriterBase, IReadOnlyCollection<IRewritable>> additionalRewrites)
+        : base(additionalRewrites)
+    {
+    }
+
+    public override SyntaxNode? VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+      var symbol = ModelExtensions.GetDeclaredSymbol(CurrentMethod.SemanticModel, node);
+      var parents = CurrentMethod.Parents.Select(p => p.From).ToArray();
+      var parametersToAnnotate = new List<int>();
+      for (var i = 0; i < node.ParameterList.Parameters.Count; i++)
+      {
+        foreach (var parent in parents)
+        {
+          if (parent is Method parentMethod)
+          {
+            var isArgumentPossiblyNull = IsArgumentPossiblyNull(parentMethod, i);
+            if (isArgumentPossiblyNull)
+            {
+              parametersToAnnotate.Add(i);
+            }
+          }
+        }
+      }
+
+      if (parametersToAnnotate.Count == 0)
+        return node;
+
+      var newList = node.ParameterList;
+
+      foreach (var parameterIndex in parametersToAnnotate)
+      {
+        var existingParameter = newList.Parameters[parameterIndex];
+        newList = newList.ReplaceNode(
+            existingParameter,
+            existingParameter.WithType (NullUtilities.ToNullable (existingParameter.Type!)));
+      }
+
+      return node.WithParameterList(newList);
+    }
+
+    public bool IsArgumentPossiblyNull(Method method, int argumentIndex)
+    {
+      var syntax = method.MethodDeclaration;
+
+      if (syntax.Body is null)
+        return false;
+
+      var invocations = syntax.Body.DescendantNodes().Where(n => n.IsKind(SyntaxKind.InvocationExpression)).Cast<InvocationExpressionSyntax>().ToArray();
+
+      return invocations.Any(i => NullUtilities.CanBeNull(i.ArgumentList.Arguments[argumentIndex].Expression, CurrentMethod.SemanticModel));
+    }
+  }
+}
---
+++ Unittests/Analysis/MethodArgumentRewriterTest.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NullableReferenceTypesRewriter.Analysis;
+using NUnit.Framework;
+
+namespace NullableReferenceTypesRewriter.UnitTests.Analysis
+{
+  public class MethodArgumentRewriterTest
+  {
+    [Test]
+    public void SingleParameter_NullArgument_Nullable()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string? something)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(null);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void SingleParameter_AlreadyNullable_Unchanged()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string? something)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string? something)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(null);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void SingleParameter_NotNull_Unchanged()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string something)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(string.Empty);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void MultipleParameter_NotNull_Unchanged()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string something, string somethingElse)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something, string somethingElse)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(string.Empty, string.Empty);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void MultipleParameter_FirstNull_Nullable()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string? something, string somethingElse)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something, string somethingElse)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(null, string.Empty);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void MultipleParameter_LastNull_Nullable()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string something, string? somethingElse)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something, string somethingElse)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(string.Empty, null);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    [Test]
+    public void MultipleParameter_AllNull_Nullable()
+    {
+      //language=C#
+      const string expected = @"
+public void DoStuff(string? something, string? somethingElse)
+{
+}
+";
+      var (semantic, root) = CompiledSourceFileProvider.CompileInClass (
+          "A",
+          //language=C#
+          @"
+public void DoStuff(string something, string somethingElse)
+{
+}
+
+public void CallAboveMethod()
+{
+  DoStuff(null, null);
+}
+");
+      Method method = null!;
+      var callingSyntax = (MethodDeclarationSyntax) root.DescendantNodes ().Last(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      var callingMethod = CreateMethodWrapper(callingSyntax, semantic);
+      var syntax = (MethodDeclarationSyntax) root.DescendantNodes ().First(n => n.IsKind (SyntaxKind.MethodDeclaration));
+      method = CreateMethodWrapper (syntax, semantic, () => new []{ new Dependency(() => callingMethod, () => method) });
+      var sut = new MethodArgumentRewriter((b, c) => { });
+
+      var result = sut.Rewrite (method);
+
+      Assert.That (result.ToString().Trim(), Is.EqualTo (expected.Trim()));
+    }
+
+    private Method CreateMethodWrapper (
+        MethodDeclarationSyntax syntax,
+        SemanticModel semanticModel,
+        Func<IReadOnlyCollection<Dependency>>? parents = null,
+        Func<IReadOnlyCollection<Dependency>>? children = null)
+    {
+      return new Method(
+          new SharedCompilation(semanticModel.Compilation),
+          semanticModel.GetDeclaredSymbol(syntax)!,
+          parents ?? Array.Empty<Dependency>,
+          children ?? Array.Empty<Dependency>);
+    }
+  }
+}
