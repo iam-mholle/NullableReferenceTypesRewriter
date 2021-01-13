008f1bd1e7711b7f799fc85bfd324fd1709418f9
Marcel Holle <marcel.holle@rubicon.eu>
2021-01-13 14:28:19 +0100

    Add rewriters to TestVisitor

--- Core/Analysis/TestVisitor.cs
+++ Core/Analysis/TestVisitor.cs
@@ -6,23 +6,35 @@ namespace NullableReferenceTypesRewriter.Analysis
 {
   public class TestVisitor : ParentMemberGraphVisitor
   {
-    private RewriterBase _nullReturnRewriter;
+    private readonly RewriterBase _nullReturnRewriter;
+    private readonly RewriterBase _castExpressionRewriter;
+    private readonly RewriterBase _localDeclarationRewriter;
+    private readonly RewriterBase _methodArgumentRewriter;
+    private readonly RewriterBase _uninitializedFieldRewriter;
 
     public TestVisitor (Action<RewriterBase, IReadOnlyCollection<IRewritable>> additionalRewrites)
     {
       _nullReturnRewriter = new NullReturnRewriter(additionalRewrites);
+      _castExpressionRewriter = new CastExpressionRewriter (additionalRewrites);
+      _localDeclarationRewriter = new LocalDeclarationRewriter (additionalRewrites);
+      _methodArgumentRewriter = new MethodArgumentRewriter (additionalRewrites);
+      _uninitializedFieldRewriter = new UninitializedFieldRewriter (additionalRewrites);
     }
 
     public override void VisitMethod (Method method)
     {
       // Console.WriteLine ("method: " + method.MethodDeclaration.ToString());
       method.Rewrite (_nullReturnRewriter);
+      method.Rewrite (_castExpressionRewriter);
+      method.Rewrite (_localDeclarationRewriter);
+      method.Rewrite (_methodArgumentRewriter);
       base.VisitMethod (method);
     }
 
     public override void VisitField (Field field)
     {
       // Console.WriteLine ("field: " + field.FieldDeclarationSyntax.ToString());
+      field.Rewrite (_uninitializedFieldRewriter);
       base.VisitField (field);
     }
 
