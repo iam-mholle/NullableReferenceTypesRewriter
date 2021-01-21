dd817c372e397a2ce88517ac53327847629e5995
Marcel Holle <privat@mholle.com>
2021-01-21 12:34:44 +0100

    Add TODO

--- Core/Analysis/RewriterBase.cs
+++ Core/Analysis/RewriterBase.cs
@@ -11,6 +11,7 @@ namespace NullableReferenceTypesRewriter.Analysis
   // TODO: Integrate events.
   // TODO: Document rewriter capabilities thoroughly.
   // TODO: Don't Annotate [NotNull] parameters or return types, output error instead.
+  // TODO: Add PropertyNullReturnRewriter
   public abstract class RewriterBase : CSharpSyntaxRewriter
   {
     private readonly Action<RewriterBase, IReadOnlyCollection<(IRewritable, RewriteCapability)>> _additionalRewrites;
