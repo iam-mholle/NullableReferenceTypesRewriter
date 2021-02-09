8f45c5c5760b91c0420ab6e147511cb49662f36e
Marcel Holle <privat@mholle.com>
2021-02-09 12:34:05 +0100

    Add UninitializedEventRewriter

---
+++ Core/Rewriters/UninitializedEventRewriter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NullableReferenceTypesRewriter.Analysis;
+using NullableReferenceTypesRewriter.Utilities;
+
+namespace NullableReferenceTypesRewriter.Rewriters
+{
+  public class UninitializedEventRewriter : RewriterBase
+  {
+    public UninitializedEventRewriter(Action<RewriterBase, IReadOnlyCollection<(IRewritable, RewriteCapability)>> additionalRewrites)
+        : base(additionalRewrites)
+    {
+    }
+
+    public override SyntaxNode? VisitEventFieldDeclaration(EventFieldDeclarationSyntax node)
+    {
+      if (node.Declaration.Variables.All(d => IsInitializedToNotNull(CurrentEvent.SemanticModel, d)))
+        return node;
+
+      var classSyntax = (ClassDeclarationSyntax) node.Ancestors().First(a => a.IsKind(SyntaxKind.ClassDeclaration));
+
+      if (node.Declaration.Variables.Any(d => IsInitializedToNull(CurrentEvent.SemanticModel, d)))
+        return ToNullable(node);
+
+      var constructors = classSyntax.ChildNodes()
+          .Where(n => n.IsKind(SyntaxKind.ConstructorDeclaration))
+          .Cast<ConstructorDeclarationSyntax>()
+          .ToArray();
+
+      var isInitializedToNotNull = constructors.All(c => VariableInitializedToNotNullInCtorChain(CurrentEvent.SemanticModel, c, node.Declaration.Variables.First()));
+
+      if (constructors.Length == 0 || !isInitializedToNotNull)
+        return ToNullable(node);
+
+      return node;
+
+      SyntaxNode? ToNullable(EventFieldDeclarationSyntax node) => node.WithDeclaration(node.Declaration.WithType(NullUtilities.ToNullableWithGenericsCheck(CurrentEvent.SemanticModel, classSyntax, node.Declaration.Type)));
+    }
+
+    private bool VariableInitializedToNotNullInCtorChain(SemanticModel semanticModel, ConstructorDeclarationSyntax constructor, VariableDeclaratorSyntax variable)
+    {
+      var isInitializedToNotNullInCurrent = VariableInitializedToNotNull(semanticModel, constructor, variable);
+
+      if (isInitializedToNotNullInCurrent)
+        return true;
+
+      if (constructor.Initializer is null)
+        return false;
+
+      var thisConstructorSymbol = semanticModel.GetSymbolInfo(constructor.Initializer).Symbol ?? throw new InvalidOperationException();
+      var thisConstructorSyntax = (ConstructorDeclarationSyntax) thisConstructorSymbol.DeclaringSyntaxReferences.Single().GetSyntax();
+
+      return VariableInitializedToNotNullInCtorChain(semanticModel, thisConstructorSyntax, variable);
+    }
+
+    private bool VariableInitializedToNotNull(SemanticModel semanticModel, ConstructorDeclarationSyntax constructor, VariableDeclaratorSyntax variable)
+    {
+      var assignments = constructor.DescendantNodesAndSelf().OfType<AssignmentExpressionSyntax>().ToArray();
+
+      var variableSymbol = semanticModel.GetDeclaredSymbol(variable);
+      var fieldAssignments = assignments.Where(a => SymbolEqualityComparer.Default.Equals(semanticModel.GetSymbolInfo(a.Left).Symbol, variableSymbol)).ToArray();
+
+      if (fieldAssignments.Length == 0)
+      {
+        return false;
+      }
+
+      return fieldAssignments.All(a => !NullUtilities.CanBeNull(a.Right, semanticModel));
+    }
+
+    private bool IsInitializedToNull (SemanticModel semanticModel, VariableDeclaratorSyntax variableDeclarator)
+    {
+      if (variableDeclarator.Initializer != null)
+      {
+        return NullUtilities.CanBeNull (variableDeclarator.Initializer.Value, semanticModel);
+      }
+
+      return false;
+    }
+
+    private bool IsInitializedToNotNull (SemanticModel semanticModel, VariableDeclaratorSyntax variableDeclarator)
+    {
+      if (variableDeclarator.Initializer != null)
+      {
+        return !NullUtilities.CanBeNull (variableDeclarator.Initializer.Value, semanticModel);
+      }
+
+      return false;
+    }
+  }
+}
---
+++ Unittests/Rewriters/UninitializedEventRewriterTest.cs
@@ -0,0 +1,169 @@
+﻿using NullableReferenceTypesRewriter.Rewriters;
+using NUnit.Framework;
+
+namespace NullableReferenceTypesRewriter.UnitTests.Rewriters
+{
+  [TestFixture]
+  public class UninitializedEventRewriterTest : RewriterTestBase<UninitializedEventRewriter>
+  {
+    [Test]
+    public void InlineInitialized_ToNull_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test = null;
+";
+      //language=C#
+      const string input = @"
+private event Action test = null;
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void Uninitialized_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test;
+";
+      //language=C#
+      const string input = @"
+private event Action test;
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void InlineInitialized_OneNull_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test1 = null, test2 = () => {};
+";
+      //language=C#
+      const string input = @"
+private event Action test1 = null, test2 = () => {};
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void InlineInitialized_ToNotNull_Unchanged ()
+    {
+      //language=C#
+      const string input = @"
+private event Action test = () => {};
+";
+
+      SimpleUnchangedAssertion(input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void InitializedInCtor_ToNotNull_Unchanged ()
+    {
+      //language=C#
+      const string input = @"
+private event Action test;
+
+public A()
+{
+  test = () => {};
+}
+";
+
+      SimpleUnchangedAssertion(input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void InitializedInCtor_ToNull_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test;
+";
+      //language=C#
+      const string input = @"
+private event Action test;
+
+public A()
+{
+  test = null;
+}
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void InitializedInCtorChain_ToNotNull_Unchanged ()
+    {
+      //language=C#
+      const string input = @"
+private event Action test;
+
+public A(bool _) : this()
+{
+}
+
+public A()
+{
+  test = () => {};
+}
+";
+
+      SimpleUnchangedAssertion(input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void UninitializedInOneCtor_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test;
+";
+      //language=C#
+      const string input = @"
+private event Action test;
+
+public A(bool _)
+{
+  test = () => {};
+}
+
+public A()
+{
+}
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+
+    [Test]
+    public void UninitializedInOneCtorInCtorChain_Nullable ()
+    {
+      //language=C#
+      const string expected = @"
+private event Action? test;
+";
+      //language=C#
+      const string input = @"
+private event Action test;
+
+public A(bool _) : this()
+{
+  test = () => {};
+}
+
+public A()
+{
+}
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.EventField);
+    }
+  }
+}
