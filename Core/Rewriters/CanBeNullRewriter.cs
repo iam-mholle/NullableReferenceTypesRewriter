86ae2e679774e7f1e679d1849f15b0045dac2a17
Marcel Holle <privat@mholle.com>
2021-02-10 10:34:10 +0100

    Add CanBeNullRewriter

---
+++ Core/Rewriters/CanBeNullRewriter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NullableReferenceTypesRewriter.Analysis;
+using NullableReferenceTypesRewriter.Utilities;
+
+namespace NullableReferenceTypesRewriter.Rewriters
+{
+  public class CanBeNullRewriter : RewriterBase
+  {
+    public CanBeNullRewriter(Action<RewriterBase, IReadOnlyCollection<(IRewritable, RewriteCapability)>> additionalRewrites)
+        : base(additionalRewrites)
+    {
+    }
+
+    public override SyntaxNode? VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+      var resultNode = node;
+
+      if (HasCanBeNullAttribute(node))
+      {
+        resultNode = resultNode.WithReturnType(NullUtilities.ToNullable(node.ReturnType));
+      }
+
+      var resultParameterList = node.ParameterList;
+
+      foreach (var (parameter, index) in node.ParameterList.Parameters.Select((p, i) => (p, i)))
+      {
+        if (parameter.Type is {} && IsValueType(SemanticModel, parameter.Type))
+          continue;
+
+        if (HasCanBeNullAttribute(parameter))
+        {
+          resultParameterList = resultParameterList.ReplaceNode(resultParameterList.Parameters[index].Type!, NullUtilities.ToNullable(parameter.Type!));
+        }
+      }
+
+      if (resultParameterList != node.ParameterList)
+      {
+        resultNode = resultNode.WithParameterList(resultParameterList);
+      }
+
+      return resultNode;
+    }
+
+    private static bool HasCanBeNullAttribute (MemberDeclarationSyntax node)
+    {
+      return node.AttributeLists.SelectMany (list => list.Attributes)
+          .Any (attr => attr.Name.ToString().Contains ("CanBeNull"));
+    }
+
+    private static bool HasCanBeNullAttribute (ParameterSyntax node)
+    {
+      return node.AttributeLists.SelectMany (list => list.Attributes)
+          .Any (attr => attr.Name.ToString().Contains ("CanBeNull"));
+    }
+
+    private static bool IsValueType (SemanticModel semanticModel, TypeSyntax declaration)
+    {
+      if (declaration is ArrayTypeSyntax)
+        return false;
+
+      var typeSymbol = semanticModel.GetTypeInfo (declaration).Type as INamedTypeSymbol;
+
+      return typeSymbol == null || typeSymbol.IsValueType;
+    }
+  }
+}
---
+++ Unittests/Rewriters/CanBeNullRewriterTest.cs
@@ -0,0 +1,52 @@
+﻿using NullableReferenceTypesRewriter.Rewriters;
+using NUnit.Framework;
+
+namespace NullableReferenceTypesRewriter.UnitTests.Rewriters
+{
+  public class CanBeNullRewriterTest: RewriterTestBase<CanBeNullRewriter>
+  {
+    [Test]
+    public void AnnotatedMethod_ReturnTypeNullable ()
+    {
+      //language=C#
+      const string expected = @"
+[CanBeNull]
+public object? DoStuff()
+{
+  return null;
+}
+";
+      //language=C#
+      const string input = @"
+[CanBeNull]
+public object DoStuff()
+{
+  return null;
+}
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.Method);
+    }
+
+    [Test]
+    public void AnnotatedParameter_ParameterTypeNullable ()
+    {
+      //language=C#
+      const string expected = @"
+public object DoStuff([CanBeNull] string? input)
+{
+  return null;
+}
+";
+      //language=C#
+      const string input = @"
+public object DoStuff([CanBeNull] string input)
+{
+  return null;
+}
+";
+
+      SimpleRewriteAssertion(expected, input, WrapperType.Method);
+    }
+  }
+}
